87624656320dc10f8f3be6ad35ca3234b8b2efd2
TomaszPekala222 <tomasz.pekala24@wp.pl>
2017-09-10 08:41:59 +0200

    ss

--- js/custom.js
+++ js/custom.js
@@ -76,21 +76,6 @@ navigation:false
  
 });
 
-// wow
-
-//Animation Wow code Starts
-$(function(){
-wow = new WOW({
-boxClass: 'wow',
-animateClass: 'animated',
-offset: 0,
-mobile: true,
-live :true
-})
-wow.init();
-
-});
-
 
 
 
